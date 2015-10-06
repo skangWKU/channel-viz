9a9a64c311d20703edb8beba63c72b1d588976b8
skangWKU <skangWKU@users.noreply.github.com>
2015-10-06 21:51:01 +0900

    Update follows.js

--- follows.js
+++ follows.js
@@ -163,6 +163,33 @@
 
 									graph.render();
 									
+									var ticksTreatment = 'glow';
+
+									// Define and Render X Axis (Time Values)
+									var xAxis = new Rickshaw.Graph.Axis.Time( {
+										graph: graph,
+										ticksTreatment: ticksTreatment
+									});
+									xAxis.render();
+
+									// Define and Render Y Axis (Datastream Values)
+									var yAxis = new Rickshaw.Graph.Axis.Y( {
+										graph: graph,
+										tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+										ticksTreatment: ticksTreatment
+									});
+									yAxis.render();
+
+									// Enable Datapoint Hover Values
+									var hoverDetail = new Rickshaw.Graph.HoverDetail({
+										graph: graph,
+										formatter: function(series, x, y) {
+											var swatch = '<span class="detail_swatch" style="background-color: ' + series.color + ' padding: 4px;"></span>';
+											var content = swatch + "&nbsp;&nbsp;" + parseFloat(y) + '&nbsp;&nbsp;<br>';
+											return content;
+										}
+									});
+									
 									// Build the second graph
 									var graph = new Rickshaw.Graph( {
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),

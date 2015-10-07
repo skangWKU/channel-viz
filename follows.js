0b169971a5a77eb980e3a2105b93a112fdf8bd0e
skangWKU <skangWKU@users.noreply.github.com>
2015-10-07 23:40:23 +0900

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 	var defaultKey		= 'NuUTGHs92HFefYNwK6uvTDFLf2FHNdavw661ehxO44Nbvftx', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1919500875], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'IMASS Smart Solutions', // Replaces Xively logo in the header
-		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '24hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		// dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		dataColor		= '830610', // CSS HEX value of color to represent data (omit leading #)
@@ -358,7 +358,7 @@
 					if(dataDuration != '' && dataInterval != 0) {
 						updateFeeds(data.id, thisFeedDatastreams, dataDuration, dataInterval);
 					} else {
-						updateFeeds(data.id, thisFeedDatastreams, '1day', 30);
+						updateFeeds(data.id, thisFeedDatastreams, '6hours', 30);
 					}
 				} else {
 					// Duplicate Example to Build Feed UI

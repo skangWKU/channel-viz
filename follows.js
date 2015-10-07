db3fe13c35937731f3f29a832e8a6cb31f220ad2
skangWKU <skangWKU@users.noreply.github.com>
2015-10-07 11:19:25 +0900

    Update follows.js

--- follows.js
+++ follows.js
@@ -139,7 +139,7 @@
 										name: datastream.id,
 										data: points,
 										// color: '#' + dataColor
-										color: 'brightblue'
+										color: 'DF5D31'
 									});
 
 									// Initialize Graph DOM Element
@@ -150,7 +150,7 @@
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 600,
 										height: 200,
-										renderer: 'line',
+										renderer: 'area',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										padding: {

1303b0b0c6844dea5d3d5d7b807944c581d8424f
skangWKU <skangWKU@users.noreply.github.com>
2015-10-14 14:44:23 +0900

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,7 +15,7 @@
 		defaultFeeds	= [1919500875], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'IMASS Smart Solutions', // Replaces Xively logo in the header
 		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
 		// dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		dataColor		= '830610', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0

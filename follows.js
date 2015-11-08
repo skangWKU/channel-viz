5795627a6b76e8b68e6426f92a4e55c19f98a545
skangWKU <skangWKU@users.noreply.github.com>
2015-11-08 11:23:33 +0900

    Update follows.js

--- follows.js
+++ follows.js
@@ -150,12 +150,13 @@
 									// Build Graph
 									var graph = new Rickshaw.Graph( {
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
-										// width: 600,
-										width: 800,
+										width: 600,
+										// width: 800,
 										height: 200,
 										renderer: 'area',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										// max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.max_value)),
 										padding: {
 											top: 0.02,
 											right: 0.02,

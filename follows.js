ef6d03b1907305dd7e59bc87057fdeb56e8c0e00
skangWKU <skangWKU@users.noreply.github.com>
2015-11-22 15:30:17 +0900

    Update follows.js

--- follows.js
+++ follows.js
@@ -176,7 +176,8 @@
 									// Define and Render X Axis (Time Values)
 									var xAxis = new Rickshaw.Graph.Axis.Time( {
 										graph: graph,
-										ticksTreatment: ticksTreatment
+										ticksTreatment: ticksTreatment,
+										timeFixture: new Rickshaw.Fixwtures.Time.Local()
 									});
 									xAxis.render();
 
